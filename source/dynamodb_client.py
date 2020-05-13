ebd52811ac3ad2cd9b57e5118b54f77bd2149197
David Ayala <deayalar@gmail.com>
2020-05-14 00:12:02 +0200

    Add SQS client to send the ids

--- source/dynamodb_client.py
+++ source/dynamodb_client.py
@@ -10,6 +10,7 @@ import logging
 import concurrent.futures
 
 from dynamo_dao import DynamoDAO
+from sqs_client import SqsClient
 from bs4 import BeautifulSoup
 from io import StringIO
 from datetime import timedelta, date
@@ -44,6 +45,7 @@ class ChartsLoader():
             raise ValueError("timeframe must be 'daily' or 'weekly'")
         self.timeframe = timeframe
         self.dao = DynamoDAO(local)
+        self.sqs = SqsClient()
         self.retry_unprocessed = retry_unprocessed
         if threads:
             self.threads = threads
@@ -56,6 +58,8 @@ class ChartsLoader():
 
     def get_chart_item(self, country, day, thread_id=1):
         url = self.scrapper.get_download_url(country, self.timeframe, day)
+        ids = []
+        chart = {}
         try:
             csv_chart = requests.get(url)
             if csv_chart.status_code == 200:
@@ -63,17 +67,19 @@ class ChartsLoader():
                 chart_df = pd.read_csv(csv_io, sep=",", encoding='UTF-8', skiprows=1, usecols=['Position', 'Streams', "URL"])
                 chart_df = chart_df.fillna("None")
                 chart_df['URL'] = chart_df['URL'].apply(func=lambda x: x.replace('https://open.spotify.com/track/',''))
+                ids = chart_df['URL'].tolist()
                 chart_df = chart_df.rename(columns={'Position': 'pos', 'Streams': 's', 'URL': 'id'})
                 songs = chart_df.to_dict('records')
                 if self.timeframe == "weekly":
                     d_arr = [int(i) for i in (day.split("--")[1]).split("-")] 
                     day = str(date(d_arr[0],d_arr[1],d_arr[2]) - timedelta(days=1))
                 chart = {"country": country, "day": day, "songs": songs}
-                return chart
             else:
                 logger.error("Cannot get csv 404 | thread %d: [%s, %s] url: %s" % (thread_id, country, day, url))
         except Exception as e:
             logger.error("Cannot get csv | thread %d:[%s, %s] url: %s" % (thread_id, country, day, url))
+        finally:
+            return chart, ids
 
     def get_tuple_batches(self, tuples_list):
         batches = [tuples_list[i:i + self.batch_size] for i in range(0, len(tuples_list), self.batch_size)]  
@@ -82,26 +88,31 @@ class ChartsLoader():
     def save_batch(self, tuples_list, thread_id=1):
         batches = self.get_tuple_batches(tuples_list)
         charts_to_save = []
+        ids_to_send = set()
         unprocessed_items = []
         for batch in batches:
             start = time.time()
             for country, day in batch:
                 item_tuple = (country, day)
                 #if country in ["il", "th", "ro", "vn", "za", "jp", "in"]:
-                chart = self.get_chart_item(country, day, thread_id)
+                chart, ids = self.get_chart_item(country, day, thread_id)
                 if chart:
                     charts_to_save.append(chart)
+                    ids_to_send.update(ids)
                 else:
                     unprocessed_items.append(item_tuple)
-                try:
-                    self.dao.save_batch(charts_to_save)
-                except Exception as e:
-                    print(e)
-                    logger.error("Cannot save in db thread %d [%s, %s]" % (thread_id, item_tuple))
-                    unprocessed_items.extend(batch)
+            try:
+                ids_string = ",".join(ids_to_send)
+                self.sqs.send(ids_string)
+                self.dao.save_batch(charts_to_save)
+            except Exception as e:
+                print(e)
+                logger.error("Cannot save in db thread %d [%s, %s]" % (thread_id, item_tuple))
+                unprocessed_items.extend(batch)
             end = time.time()
-            logger.info("Elapsed by thread %d: %.3f secs Last [%s, %s]" % (thread_id, (end - start), country, day))
+            logger.info("Elapsed by thread %d: %.3f secs Last saved chart [%s, %s], song id count %d" % (thread_id, (end - start), country, day, len(list(ids_to_send))))
             charts_to_save = []
+            ids_to_send = set()
 
         if self.retry_unprocessed:
             unprocessed_items = self.handle_unprocessed(unprocessed_items, thread_id)
@@ -111,13 +122,17 @@ class ChartsLoader():
         logger.info("---Handling unprocessed charts thread " + str(thread_id) + " count:" + str(len(unprocessed_items)))
         new_unprocessed = []
         for c, d in unprocessed_items:
-            chart = self.get_chart_item(c, d, thread_id)
+            chart, ids = self.get_chart_item(c, d, thread_id)
             if not chart:
                 new_unprocessed.append((c,d))
             else:
                 try:
+                    logger.info("Saving [%s, %s]" % (c, d))
+                    ids_string = ",".join(ids)
+                    self.sqs.send(ids_string, is_batch=False)
                     self.dao.save_item(data=chart)
                 except Exception as e:
+                    logger.info("Error [%s, %s]" % (c, d))
                     new_unprocessed.append((c,d))
         return new_unprocessed
 
@@ -125,7 +140,7 @@ class ChartsLoader():
         logger.info("------STARTING BATCH INSERT--------")
         self.countries, self.days = self.scrapper.scrape_selects(self.timeframe)
         t_start = time.time()
-        tuples_list = [(c,d) for c in self.countries for d in self.days]
+        tuples_list = [(c,d) for c in self.countries[:5] for d in self.days[:10]]
         if self.threads:
             logger.info("Processing in %d threads" % self.threads)
             thread_batches = [a.tolist() for a in numpy.array_split(tuples_list, self.threads)]
@@ -138,11 +153,11 @@ class ChartsLoader():
         else:
             self.failed_results=self.save_batch(tuples_list)
         t_end = time.time()
-        logger.info("Unsuccesful inserts count: %d, charts: %s" % (len(self.failed_results), str(self.failed_results)))
+        logger.info("Unsuccesful inserts, charts: %s" % str(self.failed_results))
         logger.info("FINISHED Total time %.3f secs" % (t_end - t_start))
 
 #cl = ChartsLoader(timeframe="weekly", retry_unprocessed=False, local=True)
 cl = ChartsLoader(timeframe="weekly", retry_unprocessed=True, create_table=False,
-                  local=False, batch_size=25, threads=10)
+                  local=True, batch_size=25, threads=10)
 cl.load()
 cl.failed_results
---
+++ source/sqs_client.py
@@ -0,0 +1,35 @@
+import boto3
+import time
+import secrets
+
+class SqsClient():
+    def __init__(self, region='us-west-2'):
+        self.queue_name = "test_standard"
+        self.url = "https://sqs.us-west-2.amazonaws.com/585318406306/" + self.queue_name
+        self.sqs = boto3.client('sqs')
+        self.maxBatchSize = 10
+        self.entries = []
+
+    def send(self, message, is_batch=True):
+        entry = {"Id": secrets.token_hex(24), "MessageBody": message}
+        self.entries.append(entry)
+        if not is_batch:
+            response = self.sqs.send_message(QueueUrl=self.url, MessageBody=message)
+        if len(self.entries) == self.maxBatchSize:
+            response = self.sqs.send_message_batch(QueueUrl=self.url, Entries=self.entries)
+            self.entries = []
+
+    def get(self):
+        response = self.sqs.receive_message(QueueUrl=self.url, MaxNumberOfMessages=self.maxBatchSize)
+        return response['Messages']
+
+def test():
+    sqs_client = SqsClient()
+    sqs_client.send("abc,def,ghi")
+    sqs_client.send("qwe,rty,uio")
+    sqs_client.send("asd,fgh,jkl")
+
+    data = sqs_client.get()
+    for i in data:
+        print(i["Body"].split(","))
+#test()
